ba2662d5cbbe2914de971fae3db7b34cf482ed3c
Yawo Guillaume KPOTUFE <mcguy2008@gmail.com>
2014-04-02 18:33:29 +0100

    node

---
+++ cookbooks/node/metadata.rb
@@ -0,0 +1,9 @@
+maintainer       "Tikibooth Limited"
+maintainer_email "devops@butter.com.hk"
+license          "Apache 2.0"
+description      "Installs/Configures node, npm and node server providers"
+long_description IO.read(File.join(File.dirname(__FILE__), 'README.rdoc'))
+version          "1.1.0"
+depends          "git"
+depends          "runit"
+supports         "ubuntu"
