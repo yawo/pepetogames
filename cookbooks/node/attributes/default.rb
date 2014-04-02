47a880bb3384ec04b9f2eba3bb0108df80add6b0
Yawo Guillaume KPOTUFE <mcguy2008@gmail.com>
2014-04-02 18:57:41 +0100

    node

--- cookbooks/node/attributes/default.rb
+++ cookbooks/node/attributes/default.rb
@@ -1,4 +1,4 @@
-default[:node][:revision]      = "HEAD"
+default[:node][:revision]      = "v0.10.26"
 default[:node][:repo_url]      = "https://github.com/joyent/node.git"
 default[:node][:user]          = "nodejs"
-default[:node][:packages]      = []
+default[:node][:packages]      = ['coffee-script', 'lineman','yo','generator-ultimate','less','karma-cli' ]
