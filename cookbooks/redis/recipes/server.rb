504bc0cd8d84d8990d5056ca678620fb7deba5f1
Yawo Guillaume KPOTUFE <mcguy2008@gmail.com>
2014-04-04 18:36:13 +0100

    vim

--- cookbooks/redis/recipes/server.rb
+++ cookbooks/redis/recipes/server.rb
@@ -33,10 +33,10 @@ end
 
 kill_old_service('redis-server'){ only_if{ File.exists?("/etc/init.d/redis-server") } }
 
-#runit_service "redis_server" do
+runit_service "redis_server" do
 #  run_state     node[:redis][:server][:run_state]
-#  options       node[:redis]
-#end
+  options       node[:redis]
+end
 
 announce(:redis, :server,
   :port => node[:redis][:server][:port])
